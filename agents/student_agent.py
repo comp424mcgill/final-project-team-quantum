1722f648f04fb835b83bf44306e9f39cdc999d8d
Zhiwei Li <zhiwei.li2@mail.mcgill.ca>
2022-03-06 14:10:11 -0500

    add get legal move fuc

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,7 +1,7 @@
 # Student agent: Add your own agent here
 from agents.agent import Agent
 from store import register_agent
-import sys
+import numpy as np
 
 
 @register_agent("student_agent")
@@ -38,3 +38,33 @@ class StudentAgent(Agent):
         """
         # dummy return
         return my_pos, self.dir_map["u"]
+
+    def get_legal_move(self, chess_board, my_pos, adv_pos, max_step):
+        (x_max, y_max, _) = chess_board.shape()
+        step_record = np.zeros((x_max, y_max))
+        y_list = [1, 0, -1, 0]
+        x_list = [0, 1, 0, -1]
+
+        step_record[my_pos[0]][my_pos[1]] = 1
+        step_record[adv_pos[0]][adv_pos[1]] = 2
+        next_step = [my_pos]
+
+        for i in range(max_step):
+            for j in range(len(next_step)):
+                curLoc = next_step.popleft()
+                for k in range(4):
+                    x = x_list[k]
+                    y = y_list[k]
+                    if chess_board[x][y][k] and step_record[x][y] == 0:
+                        targetLoc = (curLoc[0] + x, curLoc[1] + y)
+                        step_record[curLoc[0] + x][curLoc[1] + y] = 1
+                        next_step.append(targetLoc)
+        possible_moves = []
+        for i in range(x_max):
+            for j in range(y_max):
+                if step_record[i][j] == 1:
+                    for k in range(4):
+                        if not chess_board[x][y][k]:
+                            possible_moves.append(((i, j), k))
+
+        return possible_moves
