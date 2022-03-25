479a8a6f6c400c946e19f5cc22898ae50d337146
Zhiwei Li <zhiwei.li2@mail.mcgill.ca>
2022-03-24 21:32:50 -0400

    added Select process in mcts

--- agents/MCTS/mcts_search.py
+++ agents/MCTS/mcts_search.py
@@ -1,27 +1,80 @@
+import math
+import random
+
 from agents.MCTS.node import Node
 
 
 class MCTS:
-    def __init__(self, initial_state):
+    def __init__(self, my_pos, adv_pos, turn, dir_barrier, root_board):
         # create the root
-        self.root = Node(initial_state)
+        self.root_node = Node(my_pos, adv_pos, turn, dir_barrier)
+        self.root_board = root_board
+        # self.cur_node = self.root_node
+        self.cur_board = root_board[:][:][:]
 
-    def search(self, initial_state):
+    def update_tree(self, new_my_pos, new_adv_pos, turn, dir_barrier, new_board):
+        move = self.get_adv_move(new_my_pos, new_adv_pos, new_board)
+        self.root_node = self.find_children_node(move)
+        self.root_board = new_board
+        self.cur_board = new_board[:][:][:]
+        return
+
+    def get_adv_move(self, new_my_pos, new_adv_pos, new_board):
+        """function to calculate move according to new board and position"""
+        return
+
+    def find_children_node(self, move):
+        """function to find next node according to adv_move"""
+        return
+
+    def find_best_child(self):
+        pass
+
+    def update_cur_board(self, cur_node):
+        # update the current chess board according to the current node
+        pass
+
+    def search(self):
         for i in range(2000):
             # Select, Rollout, Backpropagation
+            selected_node = self.select(self.root_node)
             pass
 
-    def select(self, node):
-        while not node.is_terminal:
+    def select(self, node: Node):
+        while not node.get_game_result(self.cur_board):
             # if if's visited
             if node.is_visited:
                 node = self.select_best_move(node)
             else:
                 node = self.expand(node)
+            self.update_cur_board(node)
+
+        self.update_cur_board(node)
         return node
 
-    def expand(self, node):
-        pass
+    def expand(self, node: Node):
+        node.get_next_state(self.cur_board)
+        return node.children[0]
 
-    def select_best_move(self, node):
+    def select_best_move(self, node: Node):
+        """function to select the best node"""
+        best_score = float('-inf')
+        best_moves = []
+
+        for cn in node.children:
+            score_flag = 1 if cn.turn else -1
+
+            # use UCT formula to calculate the score
+            child_node_score = score_flag*cn.reward/cn.visits + math.sqrt(2*math.log(node.visits/cn.visits))
+
+            # update the best score and the list of best moves
+            if child_node_score > best_score:
+                best_score = child_node_score
+                best_moves = [cn]
+            elif child_node_score == best_score:
+                best_moves.append(cn)
+
+        return random.choice(best_moves)
+
+    def rollout(self):
         pass
--- agents/MCTS/node.py
+++ agents/MCTS/node.py
@@ -1,13 +1,121 @@
-from agents.MCTS.state import State
-
+import numpy as np
+import random
 
 class Node:
-    def __init__(self, state: State, parent=None):
-        self.state = state
+    def __init__(self, my_pos, adv_pos, turn, dir_barrier, parent=None):
+        self.my_pos = my_pos
+        self.op_pos = adv_pos
+        self.turn = turn
+        self.dir_barrier = dir_barrier
+
         self.parent = parent
         self.children = []
         self.visits = 0
         self.reward = 0
+
+    def get_next_state(self, chess_board):
+        """
+        get the next possible states of the current state
+        turn: true if the next turn is mine; false otherwise
+        """
+        cur_p_pos = self.my_pos[:]
+        another_pos = self.adv_pos[:]
+        if self.turn:
+            cur_p_pos, another_pos = another_pos, cur_p_pos
+
+        # get the dimensions of the board
+        x_list = [-1, 0, 1, 0]
+        y_list = [0, 1, 0, -1]
+        x_max = chess_board.shape[0]
+        y_max = chess_board.shape[1]
+        step_record = np.zeros((x_max, y_max))
+
+        step_record[cur_p_pos[0]][cur_p_pos[1]] = 1
+        step_record[another_pos[0]][another_pos[1]] = 2
+        next_step = [cur_p_pos]
+
+        max_step = (chess_board.shape[0] + 1) // 2
+        for i in range(max_step):
+            lens = len(next_step)
+            for j in range(lens):
+                cur_loc = next_step.pop(0)
+                for k in range(4):  # direction
+                    x = cur_loc[0] + x_list[k]
+                    y = cur_loc[1] + y_list[k]
+                    if 0 <= x < x_max and 0 <= y < y_max and step_record[x][y] == 0 and not chess_board[cur_loc[0]][cur_loc[1]][k]:
+                        step_record[x][y] = 1
+                        next_step.append((x, y))
+
+        for i in range(x_max):
+            for j in range(y_max):
+                if step_record[i][j] == 1:
+                    for k in range(4):
+                        if not chess_board[i][j][k]:
+                            if self.turn:
+                                self.children.append(Node(self.my_ops, (i, j), not self.turn, k, self))
+                            else:
+                                self.children.append(Node((i, j), self.adv_pos, not self.turn, k, self))
+        random.shuffle(self.children)
+        return
+
+    def get_game_result(self, chess_board):
+        my_pos = self.my_pos
+        adv_pos = self.adv_pos
+        # get the dimensions of the board
+        x_list = [-1, 0, 1, 0]
+        y_list = [0, 1, 0, -1]
+        x_max = chess_board.shape[0]
+        y_max = chess_board.shape[1]
+        step_record = np.zeros((x_max, y_max))
+
+        step_record[my_pos[0]][my_pos[1]] = 1
+        step_record[adv_pos[0]][adv_pos[1]] = 2
+        next_step = [my_pos]
+
+        while len(next_step) > 0:
+            lens = len(next_step)
+            for j in range(lens):
+                curLoc = next_step.pop(0)
+                for k in range(4):  # direction
+                    # check if the direction can move
+                    if chess_board[curLoc[0]][curLoc[1]][k]:
+                        continue
+
+                    x = curLoc[0] + x_list[k]
+                    y = curLoc[1] + y_list[k]
+
+                    if 0 <= x < x_max and 0 <= y < y_max:
+                        if step_record[x][y] == 2:  # meet adv
+                            return False, 0, 0
+                        elif step_record[x][y] == 0:
+                            step_record[x][y] = 1
+                            next_step.append((x, y))
+
+        next_step = [adv_pos]
+        while len(next_step) > 0:
+            lens = len(next_step)
+            for j in range(lens):
+                curLoc = next_step.pop(0)
+                for k in range(4):  # direction
+                    if chess_board[curLoc[0]][curLoc[1]][k]:
+                        continue
+                    x = curLoc[0] + x_list[k]
+                    y = curLoc[1] + y_list[k]
+                    if 0 <= x < x_max and 0 <= y < y_max \
+                            and step_record[x][y] == 0:
+                        step_record[x][y] = 2
+                        next_step.append((x, y))
+
+        my_score = 0
+        adv_score = 0
+        for i in range(x_max):
+            for j in range(y_max):
+                if step_record[i][j] == 1:
+                    my_score = my_score + 1
+                if step_record[i][j] == 2:
+                    adv_score = adv_score + 1
+
+        return True, my_score, adv_score
 
     def add_child(self, child_state):
         child = Node(child_state, self)
